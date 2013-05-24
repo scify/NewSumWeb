c0d7af5d8d0162c62e3a779691b72050e3cc317b
pgiotis <pgiotis@scify.org>
2013-05-24 18:08:32 +0300

    fix graphics

--- category.php
+++ category.php
@@ -269,7 +269,7 @@ and open the template in the editor.
                                 <table>
                                     <tr>
                                         <td>
-                                             <img src="img/rightdownloadbanner.png" class="img-polaroid">
+                                            <img src="img/rightdownloadbanner.png" class="img-polaroid">
                                         </td>
                                     </tr>
                                     <tr>  
@@ -277,7 +277,7 @@ and open the template in the editor.
                                             <br>
                                             <p class="text-center"> <a href="https://play.google.com/store/apps/details?id=gr.scify.newsum.ui" class="btn btn-primary" target="_blank">Download Now</a></p>
                                         </td>
-                                        
+
                                     </tr>
                                 </table>
 <!--                                <img src="img/rightdownloadbanner.png" class="img-polaroid">
@@ -323,33 +323,37 @@ and open the template in the editor.
 
                                     <div class="hero-unit">  
                                         <address>
-                                            <strong> SCIENCE FOR YOU NPO - <a href="http://www.scify.gr/site/en/" target="_blank">SciFY</a> </strong><br>
-                                            4, I. Varvaki str., Gizi, 114 74 Athens - Greece<br>
-                                            <abbr title="Phone">P: </abbr> +30 211 0149 973 <br>
-                                            <abbr title="Fax">F: </abbr>+30 211 0124 418<br>
-                                            <abbr title="e-mail">E: </abbr><a href="mailto:#">info @ scify.org</a><br></br>
-                                            <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
 
-                                                        <div class="modal-header">
-                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                            <h4 class="modal-title" id="myModalLabel">NewSumWeb Feedback</h4>
-                                                        </div>
-                                                        <div class="modal-body">
-
-                                                            <iframe width="640" height="1120" frameborder=0  marginheight="0" marginwidth="0"  src='https://docs.google.com/spreadsheet/viewform?formkey=dE5FTFJnMk40MnFXaEVsRmYtMkdENXc6MA#gid=0'></iframe>
-
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                        </div>
-
-                                                    </div><!-- /.modal-content -->
-                                                </div><!-- /.modal-dalog -->
-                                            </div><!-- /.modal -->
-
-                                            <a data-toggle="modal" href="#myModal" class="btn btn-primary">Give us a feedback</a>
+                                            <p class="text-center">   <a href="http://www.scify.gr/site/en/" target="_blank"><img src="img/logo trans_singleline_small.png" title="SciFY.org" width="50%" height="50%"></a></p>
+                                            <p class="text-center">  <strong> SCIENCE FOR YOU NPO </strong></p>
+                                            <p class="text-center">   4, I. Varvaki str., Gizi, 114 74 <br>Athens - Greece</p><br>
+                                            <p class="text-center"> 
+                                                <abbr title="Phone">P: </abbr> +30 211 0149 973 <br>
+                                                <abbr title="Fax">F: </abbr>+30 211 0124 418<br>
+                                                <abbr title="e-mail">E: </abbr><a href="mailto:#">info @ scify.org</a>
+                                            </p>
+                                            <!--                                            <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                                                            <div class="modal-dialog">
+                                                                                                <div class="modal-content">
+                                            
+                                                                                                    <div class="modal-header">
+                                                                                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                                                                        <h4 class="modal-title" id="myModalLabel">NewSumWeb Feedback</h4>
+                                                                                                    </div>
+                                                                                                    <div class="modal-body">
+                                            
+                                                                                                        <iframe width="640" height="1120" frameborder=0  marginheight="0" marginwidth="0"  src='https://docs.google.com/spreadsheet/viewform?formkey=dE5FTFJnMk40MnFXaEVsRmYtMkdENXc6MA#gid=0'></iframe>
+                                            
+                                                                                                    </div>
+                                                                                                    <div class="modal-footer">
+                                                                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                                                                    </div>
+                                            
+                                                                                                </div> /.modal-content 
+                                                                                            </div> /.modal-dalog 
+                                                                                        </div> /.modal 
+                                            
+                                                                                        <a data-toggle="modal" href="#myModal" class="btn btn-primary">Give us a feedback</a>-->
 
 
                                         </address>
@@ -359,11 +363,11 @@ and open the template in the editor.
 
 
 
-                                <!--                                <div class="span3">
-                                
-                                                                    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=el&amp;geocode=&amp;q=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1+114+74,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=62.99906,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4,+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1,+%CE%9A%CE%B5%CE%BD%CF%84%CF%81%CE%B9%CE%BA%CF%8C%CF%82+%CE%A4%CE%BF%CE%BC%CE%AD%CE%B1%CF%82+%CE%91%CE%B8%CE%B7%CE%BD%CF%8E%CE%BD,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;t=m&amp;ll=37.993525,23.743687&amp;spn=0.023675,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
-                                                                </div>
-                                -->
+                                <div class="span3">
+
+                                    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=el&amp;geocode=&amp;q=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1+114+74,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=62.99906,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4,+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1,+%CE%9A%CE%B5%CE%BD%CF%84%CF%81%CE%B9%CE%BA%CF%8C%CF%82+%CE%A4%CE%BF%CE%BC%CE%AD%CE%B1%CF%82+%CE%91%CE%B8%CE%B7%CE%BD%CF%8E%CE%BD,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;t=m&amp;ll=37.993525,23.743687&amp;spn=0.023675,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+                                </div>
+
 
 
                             </div>
--- newindex.php
+++ newindex.php
@@ -60,7 +60,7 @@ and open the template in the editor.
                                     <div class="item active">
                                         <img src="img/carousel/newsumontheweb1.png" alt="">
                                         <div class="carousel-caption">
-                                            <h4>Welcome to NewSum Web</h4>
+                                            <h4>Welcome to NewSum on the Web</h4>
                                             <p><i>"One news summary from many news sources...for free."</i></p>                                            
                                         </div>
                                     </div>
@@ -200,33 +200,37 @@ and open the template in the editor.
 
                                     <div class="hero-unit">  
                                         <address>
-                                            <strong> SCIENCE FOR YOU NPO - <a href="http://www.scify.gr/site/en/" target="_blank">SciFY</a> </strong><br>
-                                            4, I. Varvaki str., Gizi, 114 74 Athens - Greece<br>
-                                            <abbr title="Phone">P: </abbr> +30 211 0149 973 <br>
-                                            <abbr title="Fax">F: </abbr>+30 211 0124 418<br>
-                                            <abbr title="e-mail">E: </abbr><a href="mailto:#">info @ scify.org</a><br></br>
-                                            <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
 
-                                                        <div class="modal-header">
-                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                            <h4 class="modal-title" id="myModalLabel">NewSumWeb Feedback</h4>
-                                                        </div>
-                                                        <div class="modal-body">
-
-                                                            <iframe width="640" height="1120" frameborder=0  marginheight="0" marginwidth="0"  src='https://docs.google.com/spreadsheet/viewform?formkey=dE5FTFJnMk40MnFXaEVsRmYtMkdENXc6MA#gid=0'></iframe>
-
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                        </div>
-
-                                                    </div><!-- /.modal-content -->
-                                                </div><!-- /.modal-dalog -->
-                                            </div><!-- /.modal -->
-
-                                            <a data-toggle="modal" href="#myModal" class="btn btn-primary">Give us a feedback</a>
+                                            <p class="text-center">   <a href="http://www.scify.gr/site/en/" target="_blank"><img src="img/logo trans_singleline_small.png" title="SciFY.org" width="50%" height="50%"></a></p>
+                                            <p class="text-center">  <strong> SCIENCE FOR YOU NPO </strong></p>
+                                            <p class="text-center">   4, I. Varvaki str., Gizi, 114 74 <br>Athens - Greece</p><br>
+                                            <p class="text-center"> 
+                                                <abbr title="Phone">P: </abbr> +30 211 0149 973 <br>
+                                                <abbr title="Fax">F: </abbr>+30 211 0124 418<br>
+                                                <abbr title="e-mail">E: </abbr><a href="mailto:#">info @ scify.org</a>
+                                            </p>
+                                            <!--                                            <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                                                            <div class="modal-dialog">
+                                                                                                <div class="modal-content">
+                                            
+                                                                                                    <div class="modal-header">
+                                                                                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                                                                        <h4 class="modal-title" id="myModalLabel">NewSumWeb Feedback</h4>
+                                                                                                    </div>
+                                                                                                    <div class="modal-body">
+                                            
+                                                                                                        <iframe width="640" height="1120" frameborder=0  marginheight="0" marginwidth="0"  src='https://docs.google.com/spreadsheet/viewform?formkey=dE5FTFJnMk40MnFXaEVsRmYtMkdENXc6MA#gid=0'></iframe>
+                                            
+                                                                                                    </div>
+                                                                                                    <div class="modal-footer">
+                                                                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                                                                    </div>
+                                            
+                                                                                                </div> /.modal-content 
+                                                                                            </div> /.modal-dalog 
+                                                                                        </div> /.modal 
+                                            
+                                                                                        <a data-toggle="modal" href="#myModal" class="btn btn-primary">Give us a feedback</a>-->
 
 
                                         </address>
@@ -236,11 +240,11 @@ and open the template in the editor.
 
 
 
-                                <!--                                <div class="span3">
-                                
-                                                                    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=el&amp;geocode=&amp;q=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1+114+74,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=62.99906,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4,+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1,+%CE%9A%CE%B5%CE%BD%CF%84%CF%81%CE%B9%CE%BA%CF%8C%CF%82+%CE%A4%CE%BF%CE%BC%CE%AD%CE%B1%CF%82+%CE%91%CE%B8%CE%B7%CE%BD%CF%8E%CE%BD,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;t=m&amp;ll=37.993525,23.743687&amp;spn=0.023675,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
-                                                                </div>
-                                -->
+                                <div class="span3">
+
+                                    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=el&amp;geocode=&amp;q=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1+114+74,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=62.99906,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=%CE%99%CF%89%CE%AC%CE%BD%CE%BD%CE%BF%CF%85+%CE%92%CE%B1%CF%81%CE%B2%CE%AC%CE%BA%CE%B7+4,+%CE%91%CE%B8%CE%AE%CE%BD%CE%B1,+%CE%9A%CE%B5%CE%BD%CF%84%CF%81%CE%B9%CE%BA%CF%8C%CF%82+%CE%A4%CE%BF%CE%BC%CE%AD%CE%B1%CF%82+%CE%91%CE%B8%CE%B7%CE%BD%CF%8E%CE%BD,+%CE%95%CE%BB%CE%BB%CE%AC%CE%B4%CE%B1&amp;t=m&amp;ll=37.993525,23.743687&amp;spn=0.023675,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+                                </div>
+
 
 
                             </div>
@@ -248,45 +252,45 @@ and open the template in the editor.
 
 
 
-                        </div><!--myTabContent-->
-                    </div><!--/span-->
-                </div><!--/row-->
+                    </div><!--myTabContent-->
+                </div><!--/span-->
+            </div><!--/row-->
 
-                <hr>
+            <hr>
 
-                <footer>
-                    <p>&copy; <a href="http://www.scify.gr/site/en/" target="_blank">SciFY</a> 2013</p>
-                </footer>
+            <footer>
+                <p>&copy; <a href="http://www.scify.gr/site/en/" target="_blank" style="color: #0088cc">SciFY</a> 2013</p>
+            </footer>
 
-            </div><!--/.fluid-container-->
-
-
+        </div><!--/.fluid-container-->
 
 
-            <!-- Le javascript
-            ================================================== -->
-            <!-- Placed at the end of the document so the pages load faster -->
-            <script src="js/bootstrap/jquery-1.9.1.min.js"></script>
-            <script src="js/bootstrap/bootstrap-transition.js"></script>
-            <script src="js/bootstrap/bootstrap-alert.js"></script>
-            <script src="js/bootstrap/bootstrap-modal.js"></script>
-            <script src="js/bootstrap/bootstrap-dropdown.js"></script>
-            <script src="js/bootstrap/bootstrap-scrollspy.js"></script>
-            <script src="js/bootstrap/bootstrap-tab.js"></script>
-            <script src="js/bootstrap/bootstrap-tooltip.js"></script>
-            <script src="js/bootstrap/bootstrap-popover.js"></script>
-            <script src="js/bootstrap/bootstrap-button.js"></script>
-            <script src="js/bootstrap/bootstrap-collapse.js"></script>
-            <script src="js/bootstrap/bootstrap-carousel.js"></script>
-            <script src="js/bootstrap/bootstrap-typeahead.js"></script>
-            <script>
-                $(function(){
-                    $('.carousel').carousel({
-                        interval: 7000
-                    });    
-                });
+
+
+        <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/bootstrap/jquery-1.9.1.min.js"></script>
+        <script src="js/bootstrap/bootstrap-transition.js"></script>
+        <script src="js/bootstrap/bootstrap-alert.js"></script>
+        <script src="js/bootstrap/bootstrap-modal.js"></script>
+        <script src="js/bootstrap/bootstrap-dropdown.js"></script>
+        <script src="js/bootstrap/bootstrap-scrollspy.js"></script>
+        <script src="js/bootstrap/bootstrap-tab.js"></script>
+        <script src="js/bootstrap/bootstrap-tooltip.js"></script>
+        <script src="js/bootstrap/bootstrap-popover.js"></script>
+        <script src="js/bootstrap/bootstrap-button.js"></script>
+        <script src="js/bootstrap/bootstrap-collapse.js"></script>
+        <script src="js/bootstrap/bootstrap-carousel.js"></script>
+        <script src="js/bootstrap/bootstrap-typeahead.js"></script>
+        <script>
+            $(function(){
+                $('.carousel').carousel({
+                    interval: 7000
+                });    
+            });
     
 
-            </script>
+        </script>
     </body>
 </html>
